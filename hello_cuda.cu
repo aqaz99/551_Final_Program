dd79fe2eaf732cbccc952418767ce51f2277d78c
aqaz99 <jminerswebb@gmail.com>
2022-05-11 17:37:28 -0700

    Got good split up logic for thread work

--- hello_cuda.cu
+++ hello_cuda.cu
@@ -2,18 +2,24 @@
 
 #include <stdio.h>
 // #include <math.h>
-#define SIZE	655360000
-#define THREADS 1024
+#define SIZE	20
+#define THREADS 4
+#define BLOCKS 2 // Try and stick to even numbers
 
 // __global__ Lets compiler know we can run this function on GPU.
 __global__ void VectorAdd(int *a, int *b, int *c, unsigned int n)
-{
+{	
+	// Need to get work from where the last blocks last thread's work stops
+	int work_per_thread = (SIZE/THREADS);
+	printf("work per thread %d\n", work_per_thread);
 	// Split up work
-	unsigned int work_start = (threadIdx.x) * (SIZE/THREADS);
-	unsigned int work_stop = (threadIdx.x + 1) * (SIZE/THREADS);
+	unsigned int work_start = work_per_thread * (2 * blockIdx.x + threadIdx.x);
+	unsigned int work_stop = work_per_thread * (2 * blockIdx.x + threadIdx.x + 1);
 
-	// Don't need for loop anymore
-	// for (i=0; i < n; ++i)
+	
+
+	printf("Block [%d] thread[%d] computing from %d to %d\n", blockIdx.x, threadIdx.x, work_start, work_stop-1);
+
 	for(int i = work_start; i < work_stop; i++)
 		c[i] = a[i] + b[i];
 }
@@ -34,7 +40,7 @@ int main()
 	}
 	
 	// Run the function VectorAdd with CUDA, using 1 thread block and SIZE(1024) threads per block
-	VectorAdd <<<2, THREADS>>>(a, b, c, SIZE);
+	VectorAdd <<<BLOCKS, THREADS/BLOCKS>>>(a, b, c, SIZE);
 
 	// Like join? Or barrier?
 	cudaDeviceSynchronize();
