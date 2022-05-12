1c4ec7b0eeab0443d2daf900497a7ee24055b453
aqaz99 <jminerswebb@gmail.com>
2022-05-11 17:50:24 -0700

    even better logic for threads and blocks

--- hello_cuda.cu
+++ hello_cuda.cu
@@ -2,19 +2,28 @@
 
 #include <stdio.h>
 // #include <math.h>
-#define SIZE	20
-#define THREADS 4
+#define SIZE	200
+#define THREADS 6 // This is total thread count, not threads per block
 #define BLOCKS 2 // Try and stick to even numbers
 
 // __global__ Lets compiler know we can run this function on GPU.
 __global__ void VectorAdd(int *a, int *b, int *c, unsigned int n)
 {	
 	// Need to get work from where the last blocks last thread's work stops
-	int work_per_thread = (SIZE/THREADS);
-	printf("work per thread %d\n", work_per_thread);
+	double work_per_thread = (SIZE/THREADS);
+	int threads_per_block = THREADS / BLOCKS;
+	printf("work per thread %f\n", work_per_thread);
+
 	// Split up work
-	unsigned int work_start = work_per_thread * (2 * blockIdx.x + threadIdx.x);
-	unsigned int work_stop = work_per_thread * (2 * blockIdx.x + threadIdx.x + 1);
+	unsigned int work_start = work_per_thread * (threads_per_block * blockIdx.x + threadIdx.x);
+	unsigned int work_stop;
+
+	// Last thread pick up remainder
+	if(threadIdx.x == (threads_per_block-1) && blockIdx.x == BLOCKS-1){
+		work_stop = SIZE;
+	}else{
+		work_stop = work_per_thread * (threads_per_block * blockIdx.x + threadIdx.x + 1);
+	}
 
 	
 
@@ -22,6 +31,7 @@ __global__ void VectorAdd(int *a, int *b, int *c, unsigned int n)
 
 	for(int i = work_start; i < work_stop; i++)
 		c[i] = a[i] + b[i];
+	
 }
 
 int main()
@@ -39,7 +49,7 @@ int main()
 		c[i] = 0;
 	}
 	
-	// Run the function VectorAdd with CUDA, using 1 thread block and SIZE(1024) threads per block
+	// Run the function VectorAdd with CUDA, using 1 thread block and THREADS/BLOCK threads per block
 	VectorAdd <<<BLOCKS, THREADS/BLOCKS>>>(a, b, c, SIZE);
 
 	// Like join? Or barrier?
